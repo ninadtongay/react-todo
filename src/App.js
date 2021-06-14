128deacad34f0de8085e8885fd9680cfd7e00ec8
Ninad Tongay <ninadtongay99@gmail.com>
2021-06-14 14:21:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useReducer, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function reducer (state, action) {
+  switch (action.type) {
+    case 'add-todo':
+      return {
+        todos: [...state.todos, { text: action.text, completed: false }],
+        todoCount: state.todoCount + 1
+      }
+    case 'toggle-todo':
+      return {
+        todos: state.todos.map((t, idx) => idx === action.idx ? { ...t, completed: !t.completed } : t),
+        todoCount: state.todoCount
+      }
+    case 'delete-todo':
+      return {
+        todos: state.todos.filter((t) => t.completed !== true),
+        todoCount: state.todos.filter((t) => t.completed !== true).length
+      }
+    case 'reset-count':
+      return {
+        todos: [...state.todos],
+        todoCount: 0
+      }
+    case 'delete-all':
+      return {
+        todos: [],
+        todoCount: 0
+      }
+    default:
+      return state
+  }
 }
 
-export default App;
+function App () {
+  const [{ todos, todoCount }, dispatch] = useReducer(reducer, { todos: [], todoCount: 0 })
+  const [text, setText] = useState()
+
+  return (
+    <div className='app'>
+      <h1>Simple todo app</h1>
+      <div><h3>Number of todos: {todoCount}</h3></div>
+      <form onSubmit={e => {
+        e.preventDefault()
+        dispatch({ type: 'add-todo', text })
+        setText('')
+      }}
+      >
+        <input value={text} onChange={e => setText(e.target.value)} />
+      </form>
+
+      {todos.map((t, idx) => (
+        <div key={idx} onClick={() => dispatch({ type: 'toggle-todo', idx })} style={{ textDecoration: t.completed ? 'line-through' : '' }}> {t.text}
+        </div>
+
+      ))}
+      <button onClick={() => dispatch({ type: 'delete-todo' })}>Click here to delete tasks done</button>
+      <button onClick={() => dispatch({ type: 'reset-count' })}>Click here to reset todo count</button>
+      <button onClick={() => dispatch({ type: 'delete-all' })}>Click here to delete all todos</button>
+    </div>
+  )
+}
+
+export default App
